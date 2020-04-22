fbf4fa4f760d2474b98c804fc50f81bf2ae9e67b
Gautham <gauthamsrinivas103@gmail.com>
2020-04-22 17:08:09 -0500

    Initial version

---
+++ consumer.py
@@ -0,0 +1,65 @@
+from kafka import KafkaConsumer
+import json
+import re
+from textblob import TextBlob
+
+count = 0
+tweets = []
+
+def clean_tweet(tweet):
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+
+def get_tweet_sentiment(tweet):
+    analysis = TextBlob(clean_tweet(tweet))
+    if analysis.sentiment.polarity > 0:
+        return 'positive'
+    elif analysis.sentiment.polarity == 0:
+        return 'neutral'
+    else:
+        return 'negative'
+
+consumer = KafkaConsumer('sample')
+for message in consumer:
+    msg = json.loads (message.value)
+    if 'text' in msg:
+#print (msg['text'])
+
+        parsed_tweet = {}
+
+        parsed_tweet['text'] = msg['text']
+#print (get_tweet_sentiment(msg['text']))
+        parsed_tweet['sentiment'] = get_tweet_sentiment(msg['text'])
+
+#print (msg['retweet_count'])
+        if 1:
+            if parsed_tweet not in tweets:
+                tweets.append(parsed_tweet)
+            else:
+                tweets.append(parsed_tweet)
+
+        if count == 10:
+            count = 0
+            ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
+            print("Positive tweets percentage: {} %".format(100*len(ptweets)/len(tweets)))
+            ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
+            neutweets = [tweet for tweet in tweets if tweet['sentiment'] == 'neutral']
+            print("Negative tweets percentage: {} %".format(100*len(ntweets)/len(tweets)))
+            print("Neutral tweets percentage: {} %".format(100*(len(tweets) - len(ntweets) - len(ptweets))/len(tweets)))
+            print("\n\nPositive tweets:")
+            for tweet in ptweets[:5]:
+                print(tweet['text'])
+            print("\n\nNegative tweets:")
+            for tweet in ntweets[:5]:
+                print(tweet['text'])
+            print("\n\nNeutral tweets:")
+            for tweet in neutweets[:5]:
+                print(tweet['text'])
+
+            tweets = []
+        else:
+            count = count + 1
+
+
+
+consumer.close()
+
---
+++ producer.py
@@ -0,0 +1,54 @@
+import tweepy
+import json
+import socket
+import tweepy
+from tweepy import OAuthHandler
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+import kafka
+from kafka import KafkaProducer, KafkaClient
+
+consumer_key = 'IBldcrH9B5erhV4sHGqjOX57k'
+consumer_secret = 'w2HT6sMuqluRqNPOW6cq2yMSU40anXq2MICuOsuseWFXrFr7yT'
+access_token = '1150183553830985728-09CnvReXbvHsIlJLr9R4vojGM4SoSw'
+access_token_secret = '9tdNsHUXQdevx5aRtBuFwmtITuC1BmrQajJQY5JN4isK6'
+
+class TweetsListener (StreamListener):
+  def __init__ (self):
+    super (StreamListener, self).__init__()
+
+  def on_data (self, data):
+    try:
+      producer.send(kafka_topic, data.encode ('utf-8'))
+      msg = json.loads (data)
+      print ((msg['text']))
+      if ("trump" in msg['text']):
+        producer.send('testTopic', data.encode ('utf-8'))
+      if ("trump" in msg['text']):
+        producer.send(kafka_topic, data.encode ('utf-8'))
+#      print (data.encode ('utf-8'))
+
+      return True
+      exit (0)
+
+    except BaseException as e:
+      print ("Error occured")
+
+  def on_error (self, status):
+    print (status)
+    print ("on_error occured")
+    return True
+
+
+
+
+kafka_topic = "sample"
+producer = KafkaProducer(bootstrap_servers=['localhost:9092','localhost:9093'])
+listener = TweetsListener ()
+
+auth = OAuthHandler (consumer_key, consumer_secret)
+auth.set_access_token (access_token, access_token_secret)
+twitter_stream = Stream (auth, listener)
+twitter_stream.filter (track = ['covid19'], languages = ['en'])
+
+
